130dd15ad9b2feb1002c9cc46350705d3924b6ed
yanxiaofei <forget_love2161@126.com>
2017-11-21 00:21:21 +0800

    feat: 修改表单字段

--- src/views/admin/hao/edit/components/modal-edit.vue
+++ src/views/admin/hao/edit/components/modal-edit.vue
@@ -5,14 +5,14 @@
     :loading="loading"
     @on-ok="asyncOK">
     <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="50">
-      <Form-item label="名称" prop="name">
-        <Input v-model="formValidate.name" placeholder="请输入..."></Input>
+      <Form-item label="名称" prop="websiteName">
+        <Input v-model="formValidate.websiteName" placeholder="请输入..."></Input>
       </Form-item>
-      <Form-item label="url" prop="url">
-        <Input v-model="formValidate.url" placeholder="请输入..."></Input>
+      <Form-item label="url" prop="websiteUrl">
+        <Input v-model="formValidate.websiteUrl" placeholder="请输入..."></Input>
       </Form-item>
-      <Form-item label="logo" prop="logo">
-        <Input v-model="formValidate.logo" placeholder="请输入..."></Input>
+      <Form-item label="logo" prop="websiteLogo">
+        <Input v-model="formValidate.websiteLogo" placeholder="请输入..."></Input>
       </Form-item>
       <Form-item label="分类" prop="type">
         <Select v-model="formValidate.type" placeholder="选择分类">
@@ -21,8 +21,8 @@
             <Option value="3">论坛社区</Option>
         </Select>
       </Form-item>
-      <Form-item label="描述" prop="desc">
-        <Input v-model="formValidate.desc" type="textarea" :autosize="{minRows: 5,maxRows: 5}" placeholder="请输入..."></Input>
+      <Form-item label="描述" prop="websiteDesc">
+        <Input v-model="formValidate.websiteDesc" type="textarea" :autosize="{minRows: 5,maxRows: 5}" placeholder="请输入..."></Input>
       </Form-item>
     </Form>
   </Modal>
@@ -49,19 +49,19 @@ export default {
       loading: true,
       formValidate: {},
       ruleValidate: {
-        name: [
+        websiteName: [
           { required: true, message: '标题不能为空', trigger: 'blur' },
         ],
-        url: [
+        websiteUrl: [
           { required: true, message: 'url不能为空', trigger: 'blur' },
         ],
-        logo: [
+        websiteLogo: [
           { required: true, message: 'logo地址不能为空', trigger: 'blur' },
         ],
         type: [
           { required: true, message: '选择分类', trigger: 'change' },
         ],
-        desc: [
+        websiteDesc: [
           { required: true, message: '请输入详情', trigger: 'blur' },
           { type: 'string', min: 1, message: '介绍不能少于20字', trigger: 'blur' },
         ],
@@ -80,16 +80,33 @@ export default {
     },
   },
   methods: {
+    /**
+     * 表单验证
+     */
     asyncOK() {
       this.$refs['formValidate'].validate((valid) => {
         if (valid) {
-          console.log('ssss')
+          this.postWebsite()
         } else {
           this.loading = false
           setTimeout(() => {
             this.loading = true
           }, 10)
         }
+      })
+    },
+    /**
+     * 提交填写的数据
+     */
+    postWebsite() {
+      this.$ajax.haoAdd({
+        data: this.formValidate,
+        type: 'add',
+        id: '12sdfsdfsdf',
+      }).then((data) => {
+        console.log(data)
+      }).catch((err) => {
+        console.log(err)
       })
     },
   },
