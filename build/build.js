b2b4efde2109866b7672c4fd353857569870386e
yanxiaofei <forget_love2161@126.com>
2018-01-22 23:12:17 +0800

    fix: 解决“webpack中使用mavonEditor时，处理heightlight.js的打包问题”

--- build/build.js
+++ build/build.js
@@ -9,6 +9,7 @@ var chalk = require('chalk')
 var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
+const rebuildHljs = require('./rebuildHljs')
 
 var spinner = ora('building for production...')
 spinner.start()
@@ -18,6 +19,7 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   webpack(webpackConfig, function (err, stats) {
     spinner.stop()
     if (err) throw err
+    rebuildHljs()
     process.stdout.write(stats.toString({
       colors: true,
       modules: false,
