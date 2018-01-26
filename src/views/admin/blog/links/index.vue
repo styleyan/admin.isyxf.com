dfcb51b2dea8ce608d9dc1a7ca05d2646907c5d9
yanxiaofei <forget_love2161@126.com>
2018-01-26 18:05:10 +0800

    fix: 增加友情链接页面编辑

--- src/views/admin/blog/links/index.vue
+++ src/views/admin/blog/links/index.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="">
-    正在开发中~~~~~
+    <Row class="links-ivu-row">
+      <Col span="18"><Button type="error">删除</Button></Col>
+      <Col span="6">
+        <Input v-model="searchVal" placeholder="搜索内容">
+          <Button slot="append" icon="ios-search"></Button>
+        </Input>
+      </Col>
+    </Row>
+    <table-list></table-list>
   </div>
 </template>
+<script>
+import TableList from './components/table-list.vue'
+
+export default {
+  name: '',
+  components: {TableList},
+  data() {
+    return {
+      searchVal: '',
+    }
+  },
+}
+</script>
+<style lang="stylus" scoped>
+.links-ivu-row {
+  margin-buttom 16px
+}
+</style>
