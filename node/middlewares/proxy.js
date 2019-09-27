9bf528f8fb6124a0dda766d2a291620a2e787777
yanxiaofei <forget_love2161@126.com>
2019-09-28 00:42:41 +0800

    feat: 优化代理为设置cookies问题

--- node/middlewares/proxy.js
+++ node/middlewares/proxy.js
@@ -1,25 +1,28 @@
 'use strict'
 
 const fetch = require('node-fetch')
-const evn = require('../core/env')
+const host = '47.99.247.50:8080'
 
 module.exports = function () {
   return async function (ctx) {
-    const serverApi = `http://${evn.host}${ctx.request.url}`
+    const serverApi = `http://${host}${ctx.request.url}`
     const requestMethod = ctx.request.method
     const param = {
       method: requestMethod,
       headers: ctx.request.header,
     }
 
-    console.log(serverApi)
-    console.log(requestMethod)
-
     if (requestMethod === 'POST') {
       param.body = JSON.stringify(ctx.request.body)
     }
+
+    // fetch: https://www.npmjs.com/package/node-fetch
     return fetch(serverApi, param).then(res => {
+      // 获取返回cookies
+      const cookies = res.headers.raw()['set-cookie']
+      ctx.cookies.set(cookies)
       ctx.set('Content-Type', 'application/json;charset=UTF-8')
+
       ctx.body = res.body
     })
   }
